288441f7a71b3313407eb5f00a2a3ceb37be2881
jin <wlsdks12@naver.com>
2021-03-28 02:05:53 +0900

    django course 31

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form): # post 를 통해 호출되서 form 안에 image, nickname, message 정보를 가지고 있다.
+        temp_profile = form.save(commit=False) # commit 을 false로 하면 임시데이터가 저장됨!!
+        temp_profile.user = self.request.user # form 을 통해 들어온 user정보를 request 요청보낸 당사자의 user정보로 지정해줌
+        temp_profile.save() # 최종적으로 저장
+        return super().form_valid(form)
