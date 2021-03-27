283c353a20f379e0c4f17f48516a629b8b5016fa
jin <wlsdks12@naver.com>
2021-03-28 03:23:37 +0900

    django course 33

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form): # post 를 통해 호출되서 form 안에 image, nickname, message 정보를 가지고 있다.
@@ -22,6 +21,10 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user # form 을 통해 들어온 user정보를 request 요청보낸 당사자의 user정보로 지정해줌
         temp_profile.save() # 최종적으로 저장
         return super().form_valid(form)
+
+    def get_success_url(self): # pk 값 때문에 그냥 get_success_url에 값을 넣으면 연결이안됨!! 그래서 오버라이드 해주는 것임
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -31,3 +34,6 @@ class ProfileUpdateView(UpdateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
