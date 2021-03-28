16d7cb21383abaaf5eabe44a19bd59ab6d60f866
jin <wlsdks12@naver.com>
2021-03-29 03:27:40 +0900

    django course 41

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이 설정을 해줘야 프로필 이미지가 나옴 미디어와 연결시켜주는 역할의 코드이다. media에 연결됨
